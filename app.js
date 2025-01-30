cd141fd714c4a0a5764a338ff7ea256b893c7e68
navaneeth2707 <anavaneeth2707@gmail.com>
2025-01-30 06:56:22 +0530

    made a connect with database

--- app.js
+++ app.js
@@ -1,3 +1,32 @@
-const {mongoClient} =require("mongodb")
+const {MongoClient} =require("mongodb")
 const url=require("./atlas_url")
 console.log(url);
+
+const client =new MongoClient(url)
+const dbname="learndb";
+const connecToDatabase = async () => {
+    try{
+     
+        await client.connect();
+        console.log(`Connected to the ${dbname} database`);
+    }catch(err)
+    {
+        console.log(err);
+    }
+};
+
+const main =async () =>{
+    try{
+        await connecToDatabase();
+
+    }catch(err)
+    {
+        console.log("err1")
+    }
+    finally
+    {
+        await client.close();
+    }
+};
+main();
+
--- atlas_url.js
+++ atlas_url.js
@@ -1,2 +1,2 @@
 module.exports=url =
-"mongodb+srv://anavaneeth277:23@cluster0.cnd5w.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+"mongodb+srv://anavaneeth277:@cluster0.cnd5w.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
