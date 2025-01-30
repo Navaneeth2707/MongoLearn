411536b34bc561cbe4622cddf0ae41965b510cbc
navaneeth2707 <anavaneeth2707@gmail.com>
2025-01-30 07:08:41 +0530

    commented common errors

--- atlas_url.js
+++ atlas_url.js
@@ -1,2 +1,7 @@
 module.exports=url =
 "mongodb+srv://anavaneeth277:@cluster0.cnd5w.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+
+// main connection errors
+// 1. ip adress may not has access
+// 2. url string may miss ex  @ it has some value
