d44459d67bec3d854f9fec133645804bc12da529
navaneeth2707 <anavaneeth2707@gmail.com>
2025-01-30 19:38:43 +0530

    update

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const {MongoClient} =require("mongodb")
+const { ObjectId } = require("mongodb")
 const url=require("./atlas_url")
 console.log(url);
 
@@ -31,20 +32,25 @@ const accountCollection = client.db("learndb").collection("Cluster0")
 //     last_update : new Date(),
 // },
 // ]
-const documuntstofind ={balance:{ $gt:1000}}
 
+// const documuntstofind ={balance:{ $gt:1000}}
+
+const documuntstoUpadate ={_id: new ObjectId("679b78c2294989433aaf302c")} 
+const update ={ $inc:{balance:100}};
 const main =async () =>{
     try{
         await connecToDatabase();
 
-      let result=  await accountCollection.find(documuntstofind).toArray();
+    //   let result=  await accountCollection.find(documuntstofind).toArray();
        
-      let docCount =await accountCollection.countDocuments(documuntstofind)
+    //   let docCount =await accountCollection.countDocuments(documuntstofind)
 
-      await result.forEach(doc =>console.log(doc));
+    //   await result.forEach(doc =>console.log(doc));
 
-         console.log(`doc found${docCount}`);
-         console.log(result);
+    //      console.log(`doc found${docCount}`);
+    //      console.log(result);
+    let result=await accountCollection.updateOne(documuntstoUpadate,update);
+    console.log(result);
     }catch(err)
     {
         console.log("err1")
--- atlas_url.js
+++ atlas_url.js
@@ -1,5 +1,5 @@
 module.exports=url =
-"mongodb+srv://anavaneeth277:qwer123@cluster0.cnd5w.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+"mongodb+srv://anavaneeth277:@cluster0.cnd5w.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
 
 // main connection errors
