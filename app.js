81072fa00625197a72341a57e6e9bf9e83ce4a44
navaneeth2707 <anavaneeth2707@gmail.com>
2025-01-30 18:26:43 +0530

    inserted using insertone

--- app.js
+++ app.js
@@ -2,11 +2,12 @@ const {MongoClient} =require("mongodb")
 const url=require("./atlas_url")
 console.log(url);
 
+
 const client =new MongoClient(url)
 const dbname="learndb";
 const connecToDatabase = async () => {
     try{
-     
+        
         await client.connect();
         console.log(`Connected to the ${dbname} database`);
     }catch(err)
@@ -14,11 +15,22 @@ const connecToDatabase = async () => {
         console.log(err);
     }
 };
+const accountCollection = client.db("learndb").collection("Cluster0")
+const sampleData= 
+{
+    accuunt_name : "navaneeth",
+    aid : "QQKMKM",
+    type : "saving",
+    balance : 10000,
+    last_update : new Date(),
+
+}
 
 const main =async () =>{
     try{
         await connecToDatabase();
-
+        let result=await accountCollection.insertOne(sampleData);
+         console.log(result);
     }catch(err)
     {
         console.log("err1")
